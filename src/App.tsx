011415fabecce418a6a2bc7b3ecbdb884c26ec6c
Alexandru Grigoroi <alexandru@grigoroi.com>
2017-07-04 00:21:40 +0100

    Implement Conway's game of life

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
 import './App.css';
+import { GameState, Cell } from './conways-game-of-life';
+import { Set } from 'immutable';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component<{}, {}> {
+  state = new State({ cells: Set.of(new Cell({ x: 1, y: 1 }))});
+
   render() {
+    console.log(this.state.next().toJS());
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p className="App-intro"> Hello </p>
       </div>
     );
   }
---
+++ src/conways-game-of-life.ts
@@ -0,0 +1,84 @@
+import { Record, Set } from 'immutable';
+
+interface CellBase {
+    readonly x: number;
+    readonly y: number;
+}
+
+const CellRecord = Record({
+    x: 0,
+    y: 0
+});
+
+const deltas = [
+    { dx: -1, dy: -1 },
+    { dx: -1, dy: 0 },
+    { dx: -1, dy: +1 },
+    { dx: 0, dy: -1 },
+    //  { dx:  0, dy:  0 },
+    { dx: 0, dy: +1 },
+    { dx: +1, dy: -1 },
+    { dx: +1, dy: 0 },
+    { dx: +1, dy: +1 }
+];
+
+export class Cell extends CellRecord implements CellBase {
+    readonly x: number;
+    readonly y: number;
+
+    constructor(props: CellBase) {
+        super(props);
+    }
+
+    neighbours(): Set<Cell> {
+        const neighbours = new Array<Cell>();
+        for (let { dx, dy } of deltas) {
+            neighbours.push(new Cell({ x: this.x + dx, y: this.y + dy }));
+        }
+        return Set(neighbours);
+    }
+
+    aliveNeighbours(aliveCells: Set<Cell>): number {
+        return this.neighbours().count(function (neighbour: Cell) {
+            return aliveCells.contains(neighbour);
+        });
+    }
+}
+
+interface GameBase {
+    readonly cells: Set<Cell>;
+}
+
+const GameRecord = Record({
+    cells: Set<Cell>(),
+});
+
+export class GameState extends GameRecord implements GameBase {
+    readonly cells: Set<Cell>;
+
+    constructor(props: GameBase) {
+        super(props);
+    }
+
+    next(): GameState {
+        const aliveCells = this.cells;
+
+        const aliveInNext = aliveCells.flatMap(function (cell: Cell) {
+            const neighbours = cell.neighbours();
+
+            const neighboursInNext = neighbours.filter(function (neighbour: Cell) {
+                return neighbour.aliveNeighbours(aliveCells) === 3;
+            }).toSet();
+
+            const aliveNeighbours = cell.aliveNeighbours(aliveCells);
+
+            if (aliveNeighbours === 2 || aliveNeighbours === 3) {
+                return neighboursInNext.add(cell);
+            } else {
+                return neighboursInNext;
+            }
+        }).toSet();
+
+        return new GameState({ cells: aliveInNext });
+    }
+}
